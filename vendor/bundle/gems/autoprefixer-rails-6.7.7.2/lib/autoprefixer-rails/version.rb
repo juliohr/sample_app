cc5d1a36c23e19b9e72782f2c0deb53f93443b14
Julio <send.to.julio@gmail.com>
2017-05-06 10:54:24 -0300

    stop tracking vendor/bundle files

--- vendor/bundle/gems/autoprefixer-rails-6.7.7.2/lib/autoprefixer-rails/version.rb
+++
@@ -1,3 +0,0 @@
-module AutoprefixerRails
-  VERSION = '6.7.7.2'.freeze unless defined? AutoprefixerRails::VERSION
-end
--- vendor/bundle/gems/rails-controller-testing-0.1.1/lib/rails/controller/testing/version.rb
+++
@@ -1,7 +0,0 @@
-module Rails
-  module Controller
-    module Testing
-      VERSION = "0.1.1"
-    end
-  end
-end
